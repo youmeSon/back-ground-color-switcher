76d0f3e70241f12319ea34b31ddb90ce5c5c66c2
youmeSon <rosydarling@naver.com>
2020-05-06 17:47:42 +0100

    Completed.

---
+++ script.js
@@ -0,0 +1,48 @@
+document.querySelector('#grey').addEventListener('click', () => {
+    document.body.style.backgroundColor = "grey";
+});
+document.querySelector('#white').addEventListener('click', () => {
+    document.body.style.backgroundColor = "white"
+});
+document.querySelector('#blue').addEventListener('click', () => {
+    document.body.style.backgroundColor = "blue"
+});
+document.querySelector('#yellow').addEventListener('click', () => {
+    document.body.style.backgroundColor = "yellow"
+});
+document.querySelector('#pink').addEventListener('click', () => {
+    document.body.style.backgroundColor = "pink"
+});
+document.querySelector('#orange').addEventListener('click', () => {
+    document.body.style.backgroundColor = "orange"
+});
+document.querySelector('#purple').addEventListener('click', () => {
+    document.body.style.backgroundColor = "purple"
+});
+document.querySelector('#red').addEventListener('click', () => {
+    document.body.style.backgroundColor = "red"
+});
+document.querySelector('#gold').addEventListener('click', () => {
+    document.body.style.backgroundColor = "gold"
+});
+document.querySelector('#nick').addEventListener('click', () => {
+    document.body.style.backgroundColor = "navy"
+});
+document.querySelector('#youme').addEventListener('click', () => {
+    document.body.style.backgroundColor = "palevioletred"
+});
+document.querySelector('#laura').addEventListener('click', () => {
+    document.body.style.backgroundColor = "plum"
+});
+document.querySelector('#bh').addEventListener('click', () => {
+    document.body.style.backgroundColor = "burlywood"
+});
+document.querySelector('#jisu').addEventListener('click', () => {
+    document.body.style.backgroundColor = "salmon"
+});
+document.querySelector('#apple').addEventListener('click', () => {
+    document.body.style.backgroundColor = "aqua"
+});
+document.querySelector('#babo').addEventListener('click', () => {
+    document.body.style.backgroundColor = "brown"
+});
